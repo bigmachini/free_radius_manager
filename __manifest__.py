840a0c287dd2f1e5ae4c94b29af4fc67431468e9
bigmachini <elisha.wambiji@bigmachini.net>
2024-11-12 09:02:37 +0000

    update manifest

--- __manifest__.py
+++ __manifest__.py
@@ -6,13 +6,7 @@
     'author': 'Bigmachini Enterprises LTD',
     'depends': ['base'],
     'data': [
-        'security/security.xml',
         'security/ir.model.access.csv',
-        'views/radcheck_views.xml',
-        'views/radreply_views.xml',
-        'views/radacct_views.xml',
-        'views/radusergroup_views.xml',
-        'views/radgroup_views.xml',
     ],
     'installable': True,
     'application': True,
