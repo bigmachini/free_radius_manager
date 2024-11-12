23514e4c52ea92e0b0c05a8bad160ef5989fd737
bigmachini <elisha.wambiji@bigmachini.net>
2024-11-12 09:20:03 +0000

    refactor code

--- models/__init__.py
+++ models/__init__.py
@@ -2,9 +2,9 @@
 
 from . import radacct
 from . import radcheck
-from. import radgroupcheck
+from . import radgroupcheck
 from . import radgroupreply
-from. import radreply
+from . import radreply
 from . import radusergroup
 from . import radpostauth
 from . import nas
