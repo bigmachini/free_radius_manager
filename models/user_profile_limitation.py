7aec44957c81e8573e62e020da0fea8179c9b9f4
bigmachini <elisha.wambiji@bigmachini.net>
2024-11-15 02:27:21 +0000

    rename field and format form view

--- models/user_profile_limitation.py
+++ models/user_profile_limitation.py
@@ -9,10 +9,12 @@ class UserProfileLimitation(models.Model):
     hotspot_profile_limitation_id = fields.Many2one('radius_manager.hotspot_profile_limitation',
                                                     string="Hotspot Profile Limitation", readonly=True)
     hotspot_user_id = fields.Many2one('radius_manager.hotspot_user', string="Hotspot User", readonly=True)
+    download_speed = fields.Char(related='hotspot_profile_limitation_id.hotspot_limitation_id.rate_limit_rx',)
+    upload_speed = fields.Char(related='hotspot_profile_limitation_id.hotspot_limitation_id.rate_limit_tx',)
+    validity = fields.Char(related='hotspot_profile_limitation_id.hotspot_profile_id.validity', )
     is_activated = fields.Boolean(string="Is Activated", default=False)
 
     @api.depends('hotspot_user_id.name', 'hotspot_profile_limitation_id.name')
     def _compute_name(self):
         for record in self:
             record.name = f'{record.hotspot_user_id.name} - {record.hotspot_profile_limitation_id.name}'
-
