024e03ebe76bb1b8c47c623f170304171da407b8
bigmachini <elisha.wambiji@bigmachini.net>
2024-11-20 07:31:09 +0000

    Add start and end time and activate profile

--- models/user_profile_limitation.py
+++ models/user_profile_limitation.py
@@ -1,3 +1,5 @@
+from datetime import timedelta, datetime
+
 from odoo import models, fields, api
 
 
@@ -11,14 +13,43 @@ class UserProfileLimitation(models.Model):
     hotspot_user_id = fields.Many2one('radius_manager.hotspot_user', string="Hotspot User", readonly=True)
     download_speed = fields.Char(related='hotspot_profile_limitation_id.hotspot_limitation_id.rate_limit_rx', )
     upload_speed = fields.Char(related='hotspot_profile_limitation_id.hotspot_limitation_id.rate_limit_tx', )
-    validity = fields.Char(related='hotspot_profile_limitation_id.hotspot_profile_id.validity', )
-    is_activated = fields.Boolean(string="Is Activated", default=False)
+    validity = fields.Char(related='hotspot_profile_limitation_id.hotspot_profile_id.validity')
+    is_activated = fields.Boolean(string="Is Active", default=False)
+    start_time = fields.Datetime(string="Start Time", readonly=True)
+    end_time = fields.Datetime(string="Expiry Time", readonly=True)
     partner_id = fields.Many2one('res.partner', string='Partner', required=True,
                                  domain=[('is_kredoh_partner', '=', True)],
                                  readonly=True,
                                  default=lambda self: self.env.user.partner_id.id)
 
+    time_left = fields.Char(string="Time Left", compute='_compute_time_left', store=True)
+
+    @api.depends('end_time')
+    def _compute_time_left(self):
+        for record in self:
+            if record.end_time:
+                now = datetime.now()
+                delta = record.end_time - now
+                hours, remainder = divmod(delta.total_seconds(), 3600)
+                minutes, seconds = divmod(remainder, 60)
+                record.time_left = f"{int(hours)}h {int(minutes)}m {int(seconds)}s"
+            else:
+                record.time_left = "0h 0m 0s"
+
     @api.depends('hotspot_user_id.name', 'hotspot_profile_limitation_id.name')
     def _compute_name(self):
         for record in self:
             record.name = f'{record.hotspot_user_id.name} - {record.hotspot_profile_limitation_id.name}'
+
+    def activate_profile(self):
+        self.ensure_one()
+        self.is_activated = True
+        self.start_time = fields.Datetime.now()
+        if self.validity[-1].lower() == 'd':
+            self.end_time = self.start_time + timedelta(days=int(self.validity[:-1]))
+        elif self.validity[-1].lower() == 'm':
+            self.end_time = self.start_time + timedelta(minutes=int(self.validity[:-1]))
+        elif self.validity[-1].lower() == 'h':
+            self.end_time = self.start_time + timedelta(hours=int(self.validity[:-1]))
+
+        # self.hotspot_user_id.assign_profile_user(self.hotspot_profile_limitation_id.hotspot_profile_id.name)
